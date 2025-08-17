45ad8df7f3d5897863ce62e26d2744cbf3886747
Sebastian Messmer <messmer@cryfs.org>
2025-08-16 20:14:07 -0700

    Add AccountType marshalling

--- src/parser/chumsky/account.rs
+++ src/parser/chumsky/account.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 use chumsky::prelude::*;
 
 use crate::model::AccountType;
@@ -12,7 +14,7 @@ const fn account_type_str(account_type: AccountType) -> &'static str {
     }
 }
 
-fn account_type<'a>() -> impl Parser<'a, &'a str, AccountType> {
+fn parse_account_type<'a>() -> impl Parser<'a, &'a str, AccountType> {
     choice((
         just(account_type_str(AccountType::Assets)).to(AccountType::Assets),
         just(account_type_str(AccountType::Liabilities)).to(AccountType::Liabilities),
@@ -22,38 +24,65 @@ fn account_type<'a>() -> impl Parser<'a, &'a str, AccountType> {
     ))
 }
 
+fn marshal_account_type(account_type: AccountType, mut writer: impl Write) -> std::fmt::Result {
+    write!(writer, "{}", account_type_str(account_type))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::rstest;
     use rstest_reuse::*;
 
-    #[template]
-    fn account_type_template(
-        #[values(
-            AccountType::Assets,
-            AccountType::Liabilities,
-            AccountType::Income,
-            AccountType::Expenses,
-            AccountType::Equity
-        )]
-        expected: AccountType,
-    ) {
-    }
+    mod account_type {
+        use super::*;
 
-    #[apply(account_type_template)]
-    #[rstest]
-    fn parse_account_type(expected: AccountType) {
-        let input = account_type_str(expected);
-        let result = account_type().parse(input);
-        assert_eq!(Ok(expected), result.into_result());
-    }
+        #[template]
+        fn account_type_template(
+            #[values(
+                AccountType::Assets,
+                AccountType::Liabilities,
+                AccountType::Income,
+                AccountType::Expenses,
+                AccountType::Equity
+            )]
+            expected: AccountType,
+        ) {
+        }
 
-    #[apply(account_type_template)]
-    #[rstest]
-    fn parse_with_extra_suffix(expected: AccountType) {
-        let input = format!("{} extra", account_type_str(expected));
-        let result = account_type().parse(&input);
-        assert!(result.into_result().is_err());
+        #[apply(account_type_template)]
+        #[rstest]
+        fn parse(expected: AccountType) {
+            let input = account_type_str(expected);
+            let result = parse_account_type().parse(input);
+            assert_eq!(Ok(expected), result.into_result());
+        }
+
+        #[apply(account_type_template)]
+        #[rstest]
+        fn parse_with_extra_suffix(expected: AccountType) {
+            let input = format!("{} extra", account_type_str(expected));
+            let result = parse_account_type().parse(&input);
+            assert!(result.into_result().is_err());
+        }
+
+        #[apply(account_type_template)]
+        #[rstest]
+        fn marshal(expected: AccountType) {
+            let mut output = String::new();
+            let result = marshal_account_type(expected, &mut output);
+            assert!(result.is_ok());
+            assert_eq!(output, account_type_str(expected));
+        }
+
+        #[apply(account_type_template)]
+        #[rstest]
+        fn marshal_and_parse(expected: AccountType) {
+            let mut marshalled = String::new();
+            marshal_account_type(expected, &mut marshalled).unwrap();
+
+            let result = parse_account_type().parse(&marshalled);
+            assert_eq!(Ok(expected), result.into_result());
+        }
     }
 }
